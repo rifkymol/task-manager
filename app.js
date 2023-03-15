bf976909f0630e7bf15dcfe2e47f402b907b4afd
Rifkymoll <Rifkymaulana123@gmail.com>
2023-03-15 10:06:12 +0700

    changing response data api

--- app.js
+++ app.js
@@ -17,11 +17,7 @@ app.get('/hello', (req, res) => {
 
 app.use('/api/v1/tasks', tasks)
 
-// app.get('/api/v1/tasks')        - get all the tasks
-// app.posts('/api/v1/tasks)       - create new posts
-// app.get('/api/v1/tasks/:id')    - get single task
-// app.patch('/api/v1/tasks/:id')  - update task (will learn the different between put and patch)
-// app.delete('api/v1/tasks/:id')  - delete task
+app.use()
 
 const port = 3000
 
--- controllers/tasks.js
+++ controllers/tasks.js
@@ -3,18 +3,24 @@ const Task = require('../models/Task')
 const getAllTasks = async (req, res) => {
   try {
     const tasks = await Task.find({})
-    res.status(200).json({ tasks })
+
+    res
+      .status(200)
+      .json({ status: 'success', data: { tasks, nbHits: tasks.length } })
   } catch (error) {
-    res.status(500).json({ msg:error })
+    res.status(500).json({ status: 'fail', msg: error })
   }  
 }
 
 const createTask = async (req, res) => {
   try {
     const task = await Task.create(req.body)
-    res.status(201).json({ task })
+
+    res
+      .status(200)
+      .json({ status: 'success', data: { task } })
   } catch (error) {
-    res.status(500).json({ msg: error })
+    res.status(500).json({ status: 'fail', msg: error })
   }
 }
 
@@ -25,9 +31,12 @@ const getTask = async (req, res) => {
     if (!task) {
       return res.status(404).json({ msg: `No task with id : ${ taskID }` })
     }
-    res.status(200).json({ task })
+
+    res
+    .status(200)
+    .json({ status: 'success', data: { task } })
   } catch (error) {
-    res.status(500).json({ msg: error })
+    res.status(500).json({ status: 'fail', msg: error })
   }
 }
 
@@ -43,9 +52,11 @@ const updateTask = async (req, res) => {
       return res.status(404).json({ msg: `No task with id : ${ taskID }` })
     }
 
-    res.status(200).json({ task })
+    res
+      .status(200)
+      .json({ status: 'success', data: { task } })
   } catch (error) {
-    res.status(500).json({ msg: error })
+    res.status(500).json({ status: 'fail', msg: error })
   }
 }
 
@@ -56,10 +67,10 @@ const deleteTask = async (req, res) => {
     if (!task) {
       return res.status(404).json({ msg: `No task with id : ${taskID}` })
     }
-    // res.status(200).json({ task })
-    // res.status(200).json({ task: null, status: 'success' })
+
+    res.status(200).json({ task: null, status: 'success' })
   } catch (error) {
-    res.status(500).json({ msg: error })
+    res.status(500).json({ status: 'fail', msg: error })
   }
 }
 
